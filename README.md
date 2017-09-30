6d1dde43d8203eac46ea179048b80f134434cfc9
Rjoydip <joydipand@gmail.com>
2017-09-30 00:34:59 -0800

    file uploaded completed

--- README.md
+++ README.md
@@ -1,33 +1,14 @@
-# node-qr-generate
+# node-file-upload
 
-> QR code generate using node.js
+> Upload file in node.js
 
 ## Running Locally
 
 ```bash
-$ git clone https://github.com/Rjoydip/node-qr-generate.git # or clone your own fork
-$ cd node-qr-generate
+$ git clone https://github.com/Rjoydip/node-file-upload.git # or clone your own fork
+$ cd node-file-upload
 $ npm install
 $ npm start
 ```
 
 Your app should now be running on [localhost:3000](http://localhost:3000).
-
-## Server API
-
-### `app.get('/');`
-
-Render index.html
-
-### `app.get('/qr');`
-
-#### `Response:`
-
-* 200: Success. Payload: url image(.png) base64 encoded object.
-
-* 400: Error. Payload: error object.
-
-Generate origin qr code as `image`.
-
-***Note***
-> The response of `/qr` url is a image(.png) base64 encoded data not canvas element or string data.
--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var QRCode = require('qrcode')
+var QRCode = require('qrcode');
+//require multer for the file uploads
+var multer = require('multer');
+var multiparty = require('multiparty');
+
 var app = express();
+
+var DIR = "./uploads";
+
+//create a cors middleware
+app.use(function (req, res, next) {
+  //set headers to allow cross origin request.
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,37 +31,119 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false,
+  uploadDir: DIR
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// MULTER 
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, DIR);
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}${path.extname(file.originalname)}`);
+  }
+})
+
+//define the type of upload multer would be doing and pass in its destination, in our case, its a single file with the name photo
+var upload = multer({
+  storage: storage
+}).any();
+
+
+// ROUTES
 
 app.get('/', function (req, res) {
   res.sendFile("index.html");
 });
 
-app.get('/qr', function (req, res) {
-  QRCode.toDataURL('http://rjoydip.github.io', function (err, url) {
-    if(err) res.status(400).send({err});
-    res.status(200).send({url});
+app.post('/formidable_file_upload', function (req, res) {
+  var form = new multiparty.Form();
+  form.uploadDir = DIR;
+  form.multiples = true;
+
+  var RENAMED_FILE;
+
+  form.parse(req, function (err, fields, files) {});
+
+  form.on('progress', function (bytesReceived, bytesExpected) {
+    res.write(JSON.stringify({
+      bytesReceived,
+      bytesExpected
+    }));
+  });
+
+  form.on('file', function (name, file) {
+    RENAMED_FILE = `${Date.now()}${path.extname(file.name)}`;
+    fs.rename(file.path, path.join(form.uploadDir, "/", RENAMED_FILE));
+  });
+
+  form.on('end', function () {
+    res.write("\n");
+    res.write(JSON.stringify({
+      files: RENAMED_FILE
+    }));
+    res.end();
+  });
+
+  form.on('error', function (err) {
+    res.status(422).send({
+      err
+    })
+  });
+
+});
+
+//multer file upload function.
+app.post('/multer_file_upload', function (req, res) {
+
+  var form = new multiparty.Form();
+  form.parse(req, function(err, fields, files) {});
+
+  form.on('progress', function (bytesReceived, bytesExpected) {
+    res.write(JSON.stringify({
+      bytesReceived,
+      bytesExpected
+    }));
+  }); // for get progress
+
+  upload(req, res, function (err) {
+    if (err) {
+      // An error occurred when uploading
+      console.log(err);
+      return res.status(422).send("an Error occured")
+    }
+
+    res.write("\n");
+    // No error occured.
+    return res.write(JSON.stringify({
+      message: "File uploaded successfully"
+    }));
   });
 });
 
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.send({err});
+  res.send({
+    err
+  });
 });
 
 module.exports = app;
